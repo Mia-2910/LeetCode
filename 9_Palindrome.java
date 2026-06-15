648b812a934d55d53a3e40a7b8006dfd6c963e4f
Mia-2910 <ialwaysdothebest2006@gmail.com>
2026-06-15 09:14:44 +0700

    15/6/26

---
+++ 9_Palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+
+        int initialise = x;
+        int number = 0;
+        while (x != 0){
+            number = number*10 + x % 10;
+            x = x/10;
+        }
+        
+        if (initialise == number ){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
